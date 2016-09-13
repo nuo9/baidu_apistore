f69f15c175b30431521e54eb97ae451840f29bba
shangxy <345592384@qq.com>
2016-09-13 20:19:34 +0800

    add comments

--- src/main/java/cn/sxy/baidu/apistore/applications/AbstractApp.java
+++ src/main/java/cn/sxy/baidu/apistore/applications/AbstractApp.java
@@ -8,28 +8,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * App 抽象类
+ * App 抽象类，表示默认的值
  * Created by shangxy on 2016/9/11.
  */
 public abstract class AbstractApp implements IApp {
-
+    // 默认头： apikey
     @Override
     public Map<String, String> getHeader() {
         return new HashMap<String, String>(2) {{
             put("apikey", ApiStoreSDK.getApikey());
         }};
     }
-
+    // 默认GET方法
     @Override
     public String getMethod() {
         return ApiStoreSDK.GET;
     }
-
+    // 默认无参数
     @Override
     public Map<String, String> getParam() {
         return new HashMap<>(2);
     }
-
+    // 默认结果处理工厂
     @Override
     public IResultFactory getResultFactory() {
         return new DefaultResultFactory();
--- src/main/java/cn/sxy/baidu/apistore/applications/IApp.java
+++ src/main/java/cn/sxy/baidu/apistore/applications/IApp.java
@@ -9,14 +9,14 @@ import java.util.Map;
  * Created by shangxy on 2016/9/11.
  */
 public interface IApp {
-
+    // 请求头
     Map<String, String> getHeader();
-
+    // 请求参数
     Map<String, String> getParam();
-
+    // 接口 url
     String getBaseUrl();
-
+    // http 请求方法
     String getMethod();
-
+    // 结果处理工厂
     IResultFactory getResultFactory();
 }
